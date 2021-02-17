d48c4d438b90703de90175be897717631f4d42aa
Erik Joling <erik@hetweblokaal.nl>
2021-02-17 10:07:51 +0100

    Set post_type archive page title and content

--- app/setup.php
+++ app/setup.php
@@ -21,5 +21,36 @@ add_action( 'plugins_loaded', function() {
 	// Admin page archive indicator
 	add_filter( 'display_post_states', __NAMESPACE__ . '\add_archive_indicator_in_admin_page_list', 10, 2);
 
+	add_filter( 'post_type_archive_title', __NAMESPACE__ . '\set_archive_page_title', 10, 2 );
+	add_filter( 'get_the_archive_description', __NAMESPACE__ . '\set_archive_page_description' );
+
 }, 10);
 
+/**
+ *
+ */
+function set_archive_page_title( $title, $post_type ) {
+
+	if ( $post_type == get_post_type() ) {
+		if ( $post_id = get_post_type_archive_page() ) {
+			$title = get_post_field( 'post_title', $post_id );
+			$title = apply_filters( 'the_title', $title );
+		}
+	}
+
+	return $title;
+}
+/**
+ *
+ */
+function set_archive_page_description( $content ) {
+
+	if ( is_projects_post_type_archive() ) {
+		if ( $post_id = get_post_type_archive_page() ) {
+			$content = get_post_field( 'post_content', $post_id );
+			$content = apply_filters( 'the_content', $content );
+		}
+	}
+
+	return $content;
+}
