1024581190a91523c37e3acc6d831b65163ef7b8
Erik Joling <erik@hetweblokaal.nl>
2021-02-18 12:11:45 +0100

    Improve registering and filtering of post-type

--- app/post-type.php
+++ app/post-type.php
@@ -13,6 +13,76 @@ add_action( 'plugins_loaded', function() {
 });
 
 /**
+ * Get post type
+ */
+function get_post_type() {
+	return 'wbl_project';
+}
+
+/**
+ * Get post type name
+ */
+function get_post_type_name() {
+	return apply_filters( 'wbl_projects_post_type_name', __('Projects', 'wbl-projects') );
+}
+
+/**
+ * Get post type singular name
+ */
+function get_post_type_singular_name() {
+	return apply_filters( 'wbl_projects_post_type_singular_name', __('Project', 'wbl-projects') );
+}
+
+/**
+ * Get post type plural name
+ */
+function get_post_type_plural_name() {
+	return apply_filters( 'wbl_projects_post_type_plural_name', get_post_type_name() );
+}
+
+/**
+ * Get archive slug for archive permalink
+ */
+function get_post_type_archive_slug() {
+
+	# Set default slug
+	$archive_slug = sanitize_title(get_post_type_plural_name());
+
+	/**
+	 * If we have an archive page, overwrite the default archive slug
+	 */
+	if ( $archive_page = get_post_type_archive_page() ) {
+
+		# Make sure archive page is not "page on front"
+		if ( \get_option('page_on_front') != $archive_page ) {
+
+			# Get page slug (including parent pages)
+			if ( $archive_page_slug = get_page_uri($archive_page) ) {
+				$archive_slug = $archive_page_slug;
+
+				# We don't want no slashes at the outsides
+				$archive_slug = trim($archive_slug, '/');
+			}
+		}
+	}
+
+	# Allow theme to override the archive slug
+	return apply_filters( 'wbl_projects_post_type_archive_slug', $archive_slug );
+}
+
+/**
+ * Get single item slug for permalink
+ */
+function get_post_type_single_item_slug() {
+
+	$item_slug = sanitize_title(get_post_type_singular_name());
+
+	# Allow theme to override the item slug
+	return apply_filters( 'wbl_projects_post_type_single_item_slug', $item_slug );
+}
+
+
+/**
  * Register the Post Type
  *
  * @link https://github.com/johnbillion/extended-cpts
@@ -42,72 +112,9 @@ function register_post_type() {
 		],
 		[
 			# Override the base names used for labels:
-			'singular' => apply_filters( 'wbl_projects_post_type_singular_name', __('Project', 'wbl-projects') ),
-			'plural'   => apply_filters( 'wbl_projects_post_type_plural_name', get_post_type_name() ),
+			'singular' => get_post_type_singular_name(),
+			'plural'   => get_post_type_plural_name(),
 			'slug'     => get_post_type_archive_slug()
 		]
 	);
-}
-
-/**
- * Get post type
- */
-function get_post_type() {
-	return 'wbl_project';
-}
-
-/**
- * Get default post type handle
- */
-function get_post_type_handle() {
-	return apply_filters( 'wbl_projects_post_type_handle', 'projects' );
-}
-
-/**
- * Get post type name
- */
-function get_post_type_name() {
-	return apply_filters( 'wbl_projects_post_type_name', __('Projects', 'wbl-projects') );
-}
-
-/**
- * Get archive slug for archive permalink
- */
-function get_post_type_archive_slug() {
-
-	# Set default slug
-	$archive_slug = get_post_type_handle();
-
-	/**
-	 * If we have an archive page, overwrite the default archive slug
-	 */
-	if ( $archive_page = get_post_type_archive_page() ) {
-
-		# Make sure archive page is not "page on front"
-		if ( \get_option('page_on_front') != $archive_page ) {
-
-			# Get page slug (including parent pages)
-			$archive_page_slug = get_page_uri($archive_page);
-
-			$archive_slug = $archive_page_slug ? $archive_page_slug : $archive_slug;
-		}
-	}
-
-	# We don't want no slashes at the outsides
-	$archive_slug = trim($archive_slug, '/');
-
-	# Allow theme to override the selected archive slug
-	$archive_slug = apply_filters( 'wbl_projects_post_type_archive_slug', $archive_slug );
-
-	return $archive_slug;
-}
-
-/**
- * Get single item slug for permalink
- */
-function get_post_type_single_item_slug() {
-
-	$item_slug = get_post_type_handle();
-
-	return apply_filters( 'wbl_projects_post_type_single_item_slug', $item_slug );
 }
