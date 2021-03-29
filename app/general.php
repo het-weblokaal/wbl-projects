6db231c1e981dd4b0751e6da2a2f2c77bb7ba935
Erik Joling <erik@hetweblokaal.nl>
2021-03-29 09:43:33 +0200

    Setup plugin for internationalization

--- app/general.php
+++ app/general.php
@@ -10,13 +10,22 @@ namespace WBL_Projects;
  */
 add_action( 'plugins_loaded', function() {
 
-	# Set archive title and description
+	// Set archive title and description
 	add_filter( 'post_type_archive_title', __NAMESPACE__ . '\set_archive_page_title', 10, 2 );
 	add_filter( 'get_the_archive_description', __NAMESPACE__ . '\set_archive_page_description' );
 
-	# Set the correct translated archive page slug
+	// Set the correct translated archive page slug
 	add_filter( 'pll_translated_slugs', __NAMESPACE__ . '\pll_set_archive_page_slug_translation', 10, 3 );
 }, 10);
+
+/**
+ * Setup at later hook
+ */
+add_action( 'init', function() {
+
+	// Inform WordPress of custom language directory
+	load_plugin_textdomain( 'wbl-projects', false, App::get_slug() . '/' . App::get_lang_dir() );
+} );
 
 
 /**
@@ -63,10 +72,10 @@ function pll_set_archive_page_slug_translation( $slugs, $language, &$mo ) {
 
 	$post_type = get_post_type();
 
-	# Hide translation from String Translations settings 
+	// Hide translation from String Translations settings 
 	$slugs["archive_{$post_type}"]['hide'] = true;
 
-	# Set the correct archive page
+	// Set the correct archive page
 	$slugs["archive_{$post_type}"]['translations'][$language->slug] = get_post_type_archive_slug($language->slug);
 
 	return $slugs;
--- index.php
+++ index.php
@@ -2,12 +2,12 @@
 /**
  * Plugin Name:  WBL Projects
  * Plugin URI:   https://github.com/het-weblokaal/wbl-projects
- * Description:  Basis projecten functionaliteit die we gebruiken voor meerdere projecten
+ * Description:  Robust projects plugin by Het Weblokaal
  * Version:      0.2.1
  * Author:       Author: Het Weblokaal <erik.info@hetweblokaal.nl>
  * Author URI:   https://www.hetweblokaal.nl/
  * Text Domain:  wbl-projects
- * Domain Path:  /resources/languages
+ * Domain Path:  /resources/lang
  * Requires PHP: 7
  * License:      GPLv3
  *
