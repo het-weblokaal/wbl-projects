d803687cc39935c9de544a2749f389340482127c
Erik Joling <info@ejoweb.nl>
2023-05-02 09:50:52 +0200

    Fix conflict with Slim SEO due to changed filter arguments

--- app/class-seo.php
+++ app/class-seo.php
@@ -23,16 +23,66 @@ class SEO {
 	}
 
 	/**
+	 * Get Slim SEO title
+	 * 
+	 * Copied from slim-seo/src/MetaTags/Title.php
+	 */
+	public static function slim_seo_get_title( $post_id = null ) {
+		$post_id = $post_id ?: $this->get_queried_object_id();
+		$data    = get_post_meta( $post_id, 'slim_seo', true );
+		return $data['title'] ?? '';
+	}
+
+	/**
+	 * Get Slim SEO description
+	 * 
+	 * Copied from slim-seo/src/MetaTags/Description.php
+	 */
+	public static function slim_seo_get_description( $post_id = null ) {
+		$post_id = $post_id ?: $this->get_queried_object_id();
+		$post    = get_post( $post_id );
+		if ( ! $post ) {
+			return '';
+		}
+
+		// Prevent showing description on password protected posts
+		if ( post_password_required( $post ) ) {
+			return __( 'There is no excerpt because this is a protected post.', 'slim-seo' );
+		}
+
+		// Use manual entered meta description if available.
+		$data = get_post_meta( $post_id, 'slim_seo', true );
+		if ( ! empty( $data['description'] ) ) {
+			$this->is_manual = true;
+			return $data['description'];
+		}
+
+		// Use post excerpt if available.
+		if ( $post->post_excerpt ) {
+			return $post->post_excerpt;
+		}
+
+		// Use post content (which page builders can change) at last.
+		return apply_filters( 'slim_seo_meta_description_generated', $post->post_content, $post );
+	}
+
+	/**
 	 * Slim SEO Meta Title on archive page
 	 *
 	 * @return string
 	 */
-	public static function slim_seo_meta_title_on_archive_page( $title, $title_obj ) {
+	public static function slim_seo_meta_title_on_archive_page( $title ) {
 
 		// Check if we are on archive page and if we have an assigned page
-		if ( is_post_type_archive( PostType::get_post_type() ) && PostType::get_archive_page() ) {
-		
-			$title = $title_obj->get_singular_value( PostType::get_archive_page() );
+		if ( is_post_type_archive( PostType::get_post_type() ) ) {
+
+			// Get the assigned archive page
+			$archive_post_id = PostType::get_archive_page();
+
+			if ($archive_post_id) {
+
+				$title = self::slim_seo_get_title($archive_post_id);
+			}
 		}
 
 		return $title;
@@ -46,9 +96,15 @@ class SEO {
 	public static function slim_seo_meta_description_on_archive_page( $description, $description_obj ) {
 
 		// Check if we are on archive page and if we have an assigned page
-		if ( is_post_type_archive( PostType::get_post_type() ) && PostType::get_archive_page() ) {
-		
-			$description = $description_obj->get_singular_value( PostType::get_archive_page() );
+		if ( is_post_type_archive( PostType::get_post_type() ) ) {
+
+			// Get the assigned archive page
+			$archive_post_id = PostType::get_archive_page();
+
+			if ($archive_post_id) {
+					
+				$description = self::slim_seo_get_description($archive_post_id);
+			}
 		}
 
 		return $description;
